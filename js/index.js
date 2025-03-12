71470052d7186b5c2fb40cd3dcc1cf2afa6b6d15
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-03-12 16:07:51 +0600

    add nav bar and button section with api

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
+function buttonCategories() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayButton(data.categories));
+}
 
+function displayButton(data) {
+  let buttonContainer = document.getElementById("button-container");
+
+  for (let i of data) {
+    let div = document.createElement("div");
+    div.innerHTML = `
+    <button class="btn btn-sm hover:text-white hover:bg-red-500">${i.category}</button>
+    `;
+    buttonContainer.appendChild(div);
+  }
+}
+
+buttonCategories();
