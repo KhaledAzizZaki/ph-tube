7c0d1f78fff479af24947b8311b7b6d19bd66fd0
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-03-13 15:29:22 +0600

    add change button color add show details button to show modal with video details

--- js/index.js
+++ js/index.js
@@ -7,13 +7,25 @@ function buttonCategories() {
 
 function displayButton(data) {
   let buttonContainer = document.getElementById("button-container");
-
+  // ADD BUTTON
   for (let i of data) {
     let div = document.createElement("div");
     div.innerHTML = `
-    <button onclick="loadCategoriesVideos(${i.category_id})" class="btn btn-sm hover:text-white hover:bg-red-500">${i.category}</button>
+    <button onclick="loadCategoriesVideos(${i.category_id})" class="btn btn-sm hover:text-white hover:bg-red-500 buttonBG">${i.category}</button>
     `;
     buttonContainer.appendChild(div);
+    // BUTTON COLOR CHANGE
+    const btnSm = document.querySelectorAll(".btn-sm");
+    for (let i of btnSm) {
+      i.addEventListener("click", function (event) {
+        for (let j of btnSm) {
+          j.classList.remove("bg-red-500");
+          j.classList.remove("text-white");
+        }
+        event.target.classList.add("bg-red-500");
+        event.target.classList.add("text-white");
+      });
+    }
   }
 }
 
@@ -78,6 +90,7 @@ const displayVideos = (videos) => {
             <p class="text-gray-500">${video.others.views} view</p>
           </div>
         </div>
+        <button onclick="loadVideosDetails('${video.video_id}')" class="btn btn-block">Show Details</button>
       </div>
     `;
     videosContainer.appendChild(div);
@@ -94,3 +107,41 @@ function loadCategoriesVideos(id) {
     .then((res) => res.json())
     .then((data) => displayVideos(data.category));
 }
+
+const loadVideosDetails = async (id) => {
+  // console.log(id);
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  videoDetails(data.video);
+};
+
+const videoDetails = (details) => {
+  document.getElementById("my_modal_1").showModal();
+
+  const videoDetailsContainer = document.getElementById(
+    "videoDetailsContainer"
+  );
+  videoDetailsContainer.innerHTML = `
+  <div class="card bg-base-100 image-full w-96 shadow-sm">
+  <figure>
+    <img
+      src="${details.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${details.title}</h2>
+    <p>${details.description.slice(1, 100)}</p>
+    <div class="card-actions justify-end">
+      <div class="modal-action">
+          <form method="dialog">
+            <!-- if there is a button in form, it will close the modal -->
+            <button class="btn">Close</button>
+          </form>
+        </div>
+    </div>
+  </div>
+  
+</div>
+  `;
+};
